ed8574bb4794e13deb77d97edfeb2d7933accf17
ziyi127 <ziyihed@outlook.com>
2025-10-19 17:31:54 +0800

    refactor: 移除未使用的tray_manager依赖及相关代码
    清理不再使用的tray_manager包及其相关代码，简化系统托盘功能实现

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,6 @@ import 'package:time_widgets/screens/home_screen.dart';
 import 'package:time_widgets/screens/timetable_edit_screen.dart';
 import 'package:time_widgets/services/system_tray_service.dart';
 import 'package:bitsdojo_window/bitsdojo_window.dart';
-import 'package:tray_manager/tray_manager.dart';
 import 'dart:io';
 
 void main() async {
@@ -41,13 +40,12 @@ class DesktopWrapper extends StatefulWidget {
   State<DesktopWrapper> createState() => _DesktopWrapperState();
 }
 
-class _DesktopWrapperState extends State<DesktopWrapper> with TrayListener {
+class _DesktopWrapperState extends State<DesktopWrapper> {
   final GlobalKey<NavigatorState> navigatorKey = GlobalKey<NavigatorState>();
   
   @override
   void initState() {
     super.initState();
-    trayManager.addListener(this);
     
     // Initialize system tray service
     SystemTrayService().initialize(() {
@@ -58,53 +56,6 @@ class _DesktopWrapperState extends State<DesktopWrapper> with TrayListener {
         ),
       );
     });
-    
-    // Setup system tray menu
-    _setupTrayMenu();
-  }
-  
-  // 设置系统托盘菜单
-  Future<void> _setupTrayMenu() async {
-    try {
-      Menu menu = Menu(
-        items: [
-          MenuItem(label: '显示窗口', onClick: (_) => appWindow.show()),
-          MenuItem(label: '编辑课表', onClick: (_) {
-            // 导航到课表编辑页面
-            navigatorKey.currentState?.push(
-              MaterialPageRoute(
-                builder: (context) => const TimetableEditScreen(),
-              ),
-            );
-            appWindow.show();
-          }),
-          MenuItem.separator(),
-          MenuItem(label: '退出', onClick: (_) => exit(0)),
-        ],
-      );
-      
-      await trayManager.setContextMenu(menu);
-      await trayManager.setToolTip('智慧课程表');
-    } catch (e) {
-      // 托盘初始化失败，静默处理避免影响应用启动
-      debugPrint('Tray initialization failed: $e');
-    }
-  }
-
-  @override
-  void dispose() {
-    trayManager.removeListener(this);
-    super.dispose();
-  }
-
-  @override
-  void onTrayIconMouseDown() async {
-    // 点击托盘图标时显示/隐藏窗口
-    if (appWindow.isVisible) {
-      appWindow.hide();
-    } else {
-      appWindow.show();
-    }
   }
 
   @override
